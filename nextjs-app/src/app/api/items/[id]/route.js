2469078655f0b8b9dbc7d3a9be7a703af5911294
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-03-17 04:13:28 +0600

    CRUD operation by nextjs route

--- nextjs-app/src/app/api/items/[id]/route.js
+++ nextjs-app/src/app/api/items/[id]/route.js
@@ -1,6 +1,43 @@
+
+
+
+import dbConnect from "@/lib/dbConnect";
+import { ObjectId } from "mongodb";
+
+
+// export async function GET(req, {params}) {
+//  const p = await params;
+//  console.log(p)
+//   return Response.json({ params : p })
+// }
+
 export async function GET(req, {params}) {
   
  const p = await params;
- console.log(p)
-  return Response.json({ params : p })
+ const singleData = await dbConnect("items").findOne({_id : new ObjectId(p.id)});
+  return Response.json({ singleData })
 }
+
+
+export async function DELETE(req, {params}) {
+  
+ const p = await params;
+ const deletedData = await dbConnect("items").deleteOne({_id : new ObjectId(p.id)});
+  return Response.json({ deletedData })
+}
+
+export async function PATCH(req, {params}) {
+  
+ const p = await params;
+ const updatedData = await dbConnect("items").updateOne({_id : new ObjectId(p.id)}, { $set: await req.json() });
+ console.log(await req.json())
+ 
+  return Response.json({ updatedData })
+}
+
+// nextjsdb
+
+// oLhbBw8v1psaV3vp
+
+// nextjsTest
+//items
--- nextjs-app/src/app/api/items/route.js
+++ nextjs-app/src/app/api/items/route.js
@@ -1,20 +1,40 @@
-export const dynamic = 'force-static'
- 
+
+
+// export async function GET() {
+//   const data = {
+//     massage : "Hello from API route",
+//     hello : "This is a sample API route in Next.js 16"
+//   }
+
+import dbConnect from "@/lib/dbConnect";
+
+
+//   return Response.json({ data })
+// }
 export async function GET() {
-  const data = {
-    massage : "Hello from API route",
-    hello : "This is a sample API route in Next.js 16"
-  }
- 
+  const data = await dbConnect("items").find({}).toArray();
+
   return Response.json({ data })
 }
 
 
+// export async function POST(req) {
+//   const postedData = await req.json(); // Assuming the request body is JSON and contains the data you want to process
+//   console.log("Received POST data:", postedData);
+
+//   return Response.json({ postedData })
+// }
+
 export async function POST(req) {
-  const postedData = await req.json(); // Assuming the request body is JSON and contains the data you want to process
-  console.log("Received POST data:", postedData);
- 
-  return Response.json({ postedData })
+  const postedData = await req.json();
+  console.log(postedData);
+
+  const result = await dbConnect("items").insertOne(postedData);
+
+  console.log(result);
+
+  return Response.json({ result });
 }
+
 
 
