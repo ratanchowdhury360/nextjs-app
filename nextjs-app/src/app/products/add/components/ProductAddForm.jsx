acf487c9f51cb59cc02960534540c9b66238b91d
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-03-26 19:38:33 +0600

    learn about router and redirect

--- nextjs-app/src/app/products/add/components/ProductAddForm.jsx
+++ nextjs-app/src/app/products/add/components/ProductAddForm.jsx
@@ -1,82 +1,77 @@
 
 
 "use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function ProductAddForm() {
-  const handleSubmit = async (e) => {
-    e.preventDefault();
 
-    const form = e.target;
-    const productName = form.productName.value;
-    const price = form.price.value;
-    const description = form.description.value;
-    const productData = { productName, price, description };
+    const router = useRouter();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    try {
-      const response = await fetch("http://localhost:3000/api/items", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(productData),
-      });
+        const form = e.target;
+        const productName = form.productName.value;
+        const price = form.price.value;
+        const description = form.description.value;
+        const productData = { productName, price, description };
 
-      const result = await response.json();
-      console.log(result);
+       
+            const response = await fetch("http://localhost:3000/api/items", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(productData),
+            });
 
-      if (response.ok) {
-        form.reset();
-        alert("Successfully added product");
-      } else {
-        alert("Failed to add product");
-      }
+            const result = await response.json();
+            console.log(result);
 
-      console.log({ productName, price, description });
-    } catch (error) {
-      console.error(error);
-      alert("Something went wrong!");
-    }
-  };
+           form.reset();
+            // alert("Successfully added product");
+            router.push("/products");
+        };
+    
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 via-pink-400 to-red-400 p-6">
-      <form
-        className="flex flex-col text-black gap-4 w-full max-w-md bg-white shadow-2xl rounded-xl p-8"
-        onSubmit={handleSubmit}
-      >
-        
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-linear-to-r from-purple-400 via-pink-400 to-red-400 p-6">
+            <form
+                className="flex flex-col text-black gap-4 w-full max-w-md bg-white shadow-2xl rounded-xl p-8"
+                onSubmit={handleSubmit}
+            >
 
-        <input
-          type="text"
-          name="productName"
-          placeholder="Product Name"
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
-        />
 
-        <input
-          type="number"
-          name="price"
-          placeholder="Price"
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
-        />
+                <input
+                    type="text"
+                    name="productName"
+                    placeholder="Product Name"
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
+                />
 
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
-        />
+                <input
+                    type="number"
+                    name="price"
+                    placeholder="Price"
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
+                />
 
-        <button
-          type="submit"
-          className="mt-4 bg-purple-600 text-white p-3 rounded-lg font-semibold hover:bg-purple-700 transition-colors"
-        >
-          Add Product
-        </button>
-      </form>
-    </div>
-  );
+                <input
+                    type="text"
+                    name="description"
+                    placeholder="Description"
+                    className="p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 placeholder-black"
+                />
+
+                <button
+                    type="submit"
+                    className="mt-4 bg-purple-600 text-white p-3 rounded-lg font-semibold hover:bg-purple-700 transition-colors"
+                >
+                    Add Product
+                </button>
+            </form>
+        </div>
+    );
 }
 
 
