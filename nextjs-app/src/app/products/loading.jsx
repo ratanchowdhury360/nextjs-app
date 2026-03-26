e2a24c33541db0c3ee7a6c914f1c47f15d9fecff
Md. Ratan Chowdhury <ratanchowdhury169@gmail.com>
2026-03-26 19:24:30 +0600

    added loading in product

---
+++ nextjs-app/src/app/products/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex items-center justify-center min-h-screen'>
+        <p className='text-bold text-2xl'>Loading...</p> 
+    </div>
+  )
+}
